afb75c087957379ca5e344de4ad9a098797e8a43
Ian Omondi <ianomondi9011@gmail.com>
2025-07-06 20:07:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const customCursor = document.querySelector('.custom-cursor');
+const navLinks = document.querySelectorAll('.navbar a');
+
+document.addEventListener('mousemove', (e) => {
+  customCursor.style.left = `${e.clientX}px`;
+  customCursor.style.top = `${e.clientY}px`;
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    customCursor.classList.add('hovered-link');
+  });
+
+  link.addEventListener('mouseleave', () => {
+    customCursor.classList.remove('hovered-link');
+  });
+});
+
+//*This is a single liine comment*//
+/* This is a multiple comment*/
+console.log ("This is Ian")
+
